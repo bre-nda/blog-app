158717c6287cc5d7812931330b9695f356faed00
bre-nda <brenda.kanale@student.moringaschool.com>
2021-11-15 14:21:17 +0300

    update the test folder

--- tests/quote_test.py
+++ tests/quote_test.py
@@ -5,7 +5,7 @@ from app.models import Quote
 class QuoteTest(unittest.TestCase):
     def setUp(self):
         """Will run before every test"""
-        self.new_quote = Quote(1,"Shalin","It's never that too serious")
+        self.new_quote = Quote(1,"Brenda","You only live once")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_quote,Quote))
